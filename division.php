3db5262a2ffc18bf6925627d043642d593bb6a05
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2020-05-23 14:47:47 +0100

    Create division.php

---
+++ division.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<!--Your code goes here-->
+<?="The result of the calculation is:"?>
+<h4><?=$_GET['numerator']/$_GET['denominator']?></h4>
+<a href="index.php">Reset</a>
+
+
+
+</body>
+</html>
