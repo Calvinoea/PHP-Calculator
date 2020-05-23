8e52529cc8d9fdb42e17ae085a2ebac3e3f97a1e
Calvinoea <39657008+Calvinoea@users.noreply.github.com>
2020-05-23 14:47:30 +0100

    Create addition.php

---
+++ addition.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<!--Your code goes here-->
+
+
+<?="The result of the calculation is:"?>
+<h4><?=$_GET['first']+$_GET['second']?></h4>
+
+<a href="index.php">Reset</a>
+</body>
+</html>
